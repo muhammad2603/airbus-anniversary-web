80246dff3be5645b907dbbf179e7a9eebf9c123a
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-25 05:12:24 +0700

    Membuat migrasi table engine_specs

---
+++ database/migrations/2026_07_24_211721_create_engine_specs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('engine_specs', function (Blueprint $table) {
+            $table->id();
+            $table->string("label", 100);
+            $table->string("thrust", 30);
+            $table->string("bypass_ration", 30);
+            $table->string("dry_weight", 30);
+            $table->unsignedInteger("type")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('engine_specs');
+    }
+};
