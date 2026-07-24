4527066cf9931ec89fa6c8b88d67edaad808f14b
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-25 06:55:00 +0700

    Membuat migrasi table engineering_pillars

---
+++ database/migrations/2026_07_24_234423_create_engineering_pillars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('engineering_pillars', function (Blueprint $table) {
+            $table->id();
+            $table->string("code", 10);
+            $table->string("name");
+            $table->string("tag", 100)->nullable();
+            $table->text("description");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('engineering_pillars');
+    }
+};
