9a2542866aa814a17f1ec3cd869ac5caaa7b6724
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-26 21:31:25 +0700

    Membuat model untuk table engineering_pillars

---
+++ app/Models/EngineeringPillarsModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Model;
+
+#[Table("engineering_pillars")]
+class EngineeringPillarsModel extends Model
+{
+    // 
+}
