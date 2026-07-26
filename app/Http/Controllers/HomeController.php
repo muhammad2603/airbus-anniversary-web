71f8c0068d250971b04a9574a345a4d493c3f544
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-27 02:16:35 +0700

    Memberikan data spesifikasi airbus a320 neo ke-halaman beranda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\EngineeringPillarsModel;
+use App\Models\SpesificationsModel;
+use Illuminate\Support\Number;
+use NumberFormatter;
 
 class HomeController extends Controller
 {
@@ -15,6 +18,16 @@ class HomeController extends Controller
             "tag",
             "description"
         ]);
-        return view('home', ["engineering_pillars" => $engineering_pillars]);
+        $env_specs_data_limit = Number::parseInt(env("SPECIFICATIONS_DATA_LIMIT"));
+        $specs = SpesificationsModel::with('typeUnit')
+            ->limit($env_specs_data_limit)
+            ->get();
+        return view(
+            'home',
+            [
+                "engineering_pillars"   => $engineering_pillars,
+                "specifications"        => $specs,
+            ]
+        );
     }
 }
